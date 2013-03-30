b6c9d5b9990c25309ce51f0eb842696d573302ca
Arjan Scherpenisse <arjan@scherpenisse.net>
2013-03-30 17:19:33 +0100

    Fix metadata

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,18 @@
 name             'zotonic'
-maintainer       'MiracleThings'
+version          '0.2.0'
+
+maintainer       'Arjan Scherpenisse'
 maintainer_email 'arjan@miraclethings.nl'
 license          'MIT'
 description      'Installs/Configures Zotonic'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+long_description "" #IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+
+depends "erlang", ">= 1.2.0"
+depends "apt", ">= 1.7.0"
+
+recipe "zotonic", "Installs Zotonic via PPA or source"
+recipe "zotonic::ppa", "Installs Zotonic via PPA"
+
+supports "ubuntu", ">= 12.04"
+supports "debian"
+
